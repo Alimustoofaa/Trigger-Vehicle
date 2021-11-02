8734c74ce3d35b2b6331aab2f68e9eeb4dec2d2c
Alimustoofaa <softhouse.ali@gmail.com>
2021-11-02 14:03:07 +0700

    read ipcam

--- src/utils/camera.py
+++ src/utils/camera.py
@@ -1,8 +1,5 @@
 import cv2
 
-from config.config import (
-	WIDTH, HEIGH
-)
 class Camera:
 	'''
 	Read camera module cv2 and
@@ -13,9 +10,6 @@ class Camera:
 	def __init__(self, url_stream):
 		self.url_stream = url_stream
 		self.camera = cv2.VideoCapture(self.url_stream)
-		self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
-		self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGH)
-		self.camera.set(cv2.CAP_PROP_FPS, 30)
 
 	def camera_streaming(self):
 		if not self.camera.isOpened(): self.__init__(self.url_stream)
